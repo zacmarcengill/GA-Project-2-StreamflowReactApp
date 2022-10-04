e565c01ef5ec0f59aaf9b798b9db3968419ebefe
zacmarcengill <zacmarcengill@gmail.com>
2022-10-03 21:17:57 -0500

    Add Mapbox

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
+import React, { useRef, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import SiteSelect from './SiteSelect';
 import Results from './Results';
-import { useState } from 'react';
 
 function App() {
 	const [site, setSite] = useState('');
 	console.log(site);
 
-	// function handleSubmit(event) {
-	// 	event.preventDefault();
-	// 	getSite(searchSite);
-	// }
-
-	// function getSite(searchSite) {
-	// 	const searchOptions =
-	// }
-
 	return (
 		<>
 			<header className='header-container'>
 				<Header />
-				<SiteSelect  />
+				<SiteSelect />
 			</header>
 			<div>
-				<Results siteInfo={site}/>
+				<Results />
 			</div>
 		</>
 	);
--- src/Results.js
+++ src/Results.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken =
+	'pk.eyJ1IjoiemFjbWFyY2VuZ2lsbCIsImEiOiJjbDh0ajN0czIwNXhpM3BwODYxbjYwbmhyIn0.odDnWTriSpnbJf3IE8vfrw';
 
 function Results() {
+	const mapContainer = useRef(null);
+	const map = useRef(null);
+	const [lng, setLng] = useState(-86);
+	const [lat, setLat] = useState(35.9);
+	const [zoom, setZoom] = useState(7);
+
+	useEffect(() => {
+		if (map.current) return; // initialize map only once
+		map.current = new mapboxgl.Map({
+			container: mapContainer.current,
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [lng, lat],
+			zoom: zoom,
+		});
+	});
+
 	return (
 		<div className='results'>
+			<div ref={mapContainer} className='map-container' />
 			<h2>Results Go Here</h2>
 		</div>
 	);
