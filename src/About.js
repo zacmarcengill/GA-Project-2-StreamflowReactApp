beab44182ea6d0bb0cc85731bf7f9291b2f5380c
zacmarcengill <zacmarcengill@gmail.com>
2022-10-05 18:00:03 -0500

    Update .css

--- src/About.js
+++ src/About.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 function About(props) {
 	return (
-		<h6 className='about'>
-			This is an app created by Zac Marcengill as a project submission during
-			the General Assembly Software Engineering Bootcamp. Developed with React,
-			Mapbox API, and streamflow data from USGS API (U.S. Geological Survey).
-			integrationCopyright 2022
-		</h6>
+		<div className='about'>
+			<h6>
+				This is an app created by Zac Marcengill as a project submission during
+				the General Assembly Software Engineering Bootcamp. Developed with
+				React, Mapbox API, and streamflow data from USGS API (U.S. Geological
+				Survey). &copy; 2022 Zac Marcengill
+			</h6>
+		</div>
 	);
 }
 
